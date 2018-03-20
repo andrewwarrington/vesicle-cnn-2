00815b08c369fb3410eb975ca4c75c71b47a4123
andrewwarrington <andrew.warrington@keble.ox.ac.uk>
2018-03-20 10:20:50 +0000

    updating license

--- vesiclecnn-2/vesicle-cnn-2.py
+++ vesiclecnn-2/vesicle-cnn-2.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2017, Probabilistic Programming Group at University of Oxford
+# Copyright (c) 2017, Andrew Warrington
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
